4972aefbeaeba8a8347901ec67e9141e10d31b5c
torik135 <riqibrekele@gmail.com>
2022-09-05 15:22:14 +0700

    add: contant for icon and tempalte text

---
+++ src/utils/constans.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchNaturalIcon from '@mui/icons-material/FaceRetouchNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon /> },
+    { name: 'Code', icon: <CodeIcon /> },
+    { name: 'Coding', icon: <CodeIcon /> },
+    { name: 'ReactJS', icon: <CodeIcon /> },
+    { name: 'NextJS', icon: <CodeIcon /> },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon /> },
+    { name: 'Podcast', icon: <GraphicEqIcon /> },
+    { name: 'Movie', icon: <OndemandVideoIcon /> },
+    { name: 'Gaming', icon: <SportsEsportsIcon /> },
+    { name: 'Live', icon: <LiveTvIcon /> },
+    { name: 'Sport', icon: <FitnessCenterIcon /> },
+    { name: 'Fashion', icon: <CheckroomIcon /> },
+    { name: 'Beauty', icon: <FaceRetouchNaturalIcon /> },
+    { name: 'Comedy', icon: <TheaterComedyIcon /> },
+    { name: 'Gym', icon: <FitnessCenterIcon /> },
+    { name: 'Crypto', icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = 'channel/thischannel';
+export const demoChannelTitle = 'this channel title';
+export const demoVideoTitle = 'this video title';
