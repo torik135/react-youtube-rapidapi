9e97692996d1f37a1dd3255b082228e4cc576f94
torik135 <riqibrekele@gmail.com>
2022-09-05 16:56:49 +0700

    add: feed UI component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from '../';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright &copy; 2022.
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: '#fff' }}
+        >
+          New <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
 };
 
 export { Feed };
