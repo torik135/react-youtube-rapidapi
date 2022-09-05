e495527f09701d48381ce793adbc7bf0ae280cfc
torik135 <riqibrekele@gmail.com>
2022-09-05 16:56:39 +0700

    add: video component template

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { SearchFeed } from './SearchFeed/SearchFeed';
 export { SearchBar } from './SearchBar/SearchBar';
 export { VideoDetail } from './VideoDetail/VideoDetail';
 export { Sidebar } from './Sidebar/Sidebar';
+export { Videos } from './Videos/Videos';
